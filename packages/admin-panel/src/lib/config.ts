92aee45682ffd804b92bc85a9c940d5ef0e3d379
Mohammad Mahdi <111296448+GodEye2004@users.noreply.github.com>
2026-02-17 00:05:56 -0800

    fix

--- packages/admin-panel/src/lib/config.ts
+++ packages/admin-panel/src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001";
-export const SERVER_API_URL = process.env.SERVER_API_URL || "http://localhost:5001";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.100.108:5001";
+export const SERVER_API_URL = process.env.SERVER_API_URL || "http://192.168.100.108:5001";
